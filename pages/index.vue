79ff433f3a0b6f2d8157da0e9d418e9e399675a2
MykolasBe <mykolas.be@gmail.com>
2020-09-22 16:46:59 +0300

    seeding

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
-    <h3>Authors</h3>
+    <table>
+      <thead>
+        <th>Order</th>
+        <th>Project</th>
+        <th>Budget</th>
+        <th>Spnt./Est./Sold</th>
+        <th>Rem. h.</th>
+        <th>Placed</th>
+      </thead>
+      <tbody>
+        <tr v-for="task in tasks" :key="task.id">
+          <td>{{ task.task_title }}</td>
+          <td>{{ task.project }}</td>
+          <td>100000$</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
     <ul>
-      <li v-for="item in author" :key="item.id">
+      <li v-for="item in tasks" :key="item.id">
         <nuxt-link :to="`/article/${item.id}`">
-          {{ item.name }}
+<!--          {{ item.task_title }}-->
         </nuxt-link>
       </li>
     </ul>
@@ -12,17 +29,22 @@
 </template>
 
 <script>
-import author from '~/apollo/queries/fetchAuthor'
+import tasks from '~/apollo/queries/fetchTask.gql'
+import times from '~/apollo/queries/fetchTime.gql'
 
 export default {
   apollo: {
-    author: {
+    tasks: {
       prefetch: true,
-      query: author
+      query: tasks
+    },
+    times: {
+      prefetch: true,
+      query: times
     }
   },
   head: {
-    title: 'Authors of Blog'
+    title: 'Teamwork Tasks'
   }
 }
 </script>
--- pages/seed.vue
+++ pages/seed.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>Import tasks</h3>
-    <button v-on:click.stop="addTasks()"> Import! </button>
+    <button v-on:click.stop="addTasks(), addSubTasks()"> Import! </button>
 
     <h3>Import time logs</h3>
     <button v-on:click.stop="addTime()"> Import! </button>
@@ -11,19 +11,34 @@
 <script>
 import taskMutation from '~/apollo/queries/upsertTasks.gql'
 import timeMutation from '~/apollo/queries/upsertTimes.gql'
+import subTaskMutation from '~/apollo/queries/upsertSubTask.gql'
 
 import taskData from '~/static/DB seed/Tasks.json'
 import timeData from "~/static/DB seed/Time.json"
 
-const tasks = taskData["todo-items"].map(function(task){
+const tasks = taskData["todo-items"].filter(function (task){return task.parentTaskId === "";}).map(
+  function(task){
   return {
     "id": task.id,
     "project": task["project-name"],
     "task_description": task.description,
     "task_title": task.content,
-    "task_time_estimate": parseInt(task.estimatedTime)
+    "task_time_estimate": parseInt(task.estimatedTime),
+    "created_at": task["created-on"],
   }
 })
+
+const subTasks = taskData["todo-items"].filter(function (task){return task.parentTaskId !== "";}).map(
+  function(task){
+    return {
+      "id": task.id,
+      "task_description": task.description,
+      "task_title": task.content,
+      "task_time_estimate": parseInt(task.estimatedTime),
+      "created_at": task["created-on"],
+      "parent_task_id": parseInt(task.parentTaskId)
+    }
+  })
 
 const times = timeData["time-entries"].map(function(timeLog){
   return {
@@ -45,6 +60,13 @@ export default {
       this.$apollo.mutate({
         mutation: taskMutation,
         variables: { objects: tasks } ,
+        // prefetch: true,
+      })
+    },
+    addSubTasks(){
+      this.$apollo.mutate({
+        mutation: subTaskMutation,
+        variables: { objects: subTasks } ,
         // prefetch: true,
       })
     },
